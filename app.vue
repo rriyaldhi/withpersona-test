d147bfa35ce23efbd893f42105c94313acfbaae8
Rizky Riyaldhi <rizky.riyaldhi@getyourguide.com>
2024-03-21 13:58:14 +0100

    move script to useServerHead

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script lang="ts" setup>
+useServerHead({
+  script: [{
+    src: "https://cdn.withpersona.com/dist/persona-v4.11.0.js",
+    type: "text/javascript",
+  }],
+});
+</script>
 <template>
   <div>Hello</div>
   <ClientOnly>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  app: {
-    head: {
-      script: [
-        { src: 'https://cdn.withpersona.com/dist/persona-v4.11.0.js', type: 'text/javascript' }
-      ],
-    }
-  },
 })
